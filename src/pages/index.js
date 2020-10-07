289b22d927dfb7c74fae9f0c5544033858b9ac60
ari7946 <mercado7946@gmail.com>
2020-10-06 21:31:35 -0700

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
-import React from "react"
+import React from "react";
+import { Link } from 'gatsby';
 
-export default function Home() {
-  return <div>Hello world!</div>
+import Layout from '../components/layout';
+
+const indexPage = () => {
+  return (
+    <Layout>
+      <h1>Hello.</h1>
+      <h2>I'm Ari, a developer living in Orange County</h2>
+      <p>Need a developer? <Link to='/contact'> Contact me.</Link></p>
+    </Layout>
+  )
 }
+
+export default indexPage;
