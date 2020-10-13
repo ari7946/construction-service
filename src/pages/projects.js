7cac47ecbf920141c8c85f21b30cb601aaa62360
ari7946 <mercado7946@gmail.com>
2020-10-13 13:31:26 -0700

    Include Links to project pages

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -24,14 +24,30 @@ const ProjectsPage = () => {
     }
   `)
   console.log('data',data)
+  const slugify = text => {
+    return text
+      .toString()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, "-")
+      .replace(/[^\w-]+/g, "")
+      .replace(/--+/g, "-")
+  }
 
   return (
     <Layout>
       <Head title="Projects" />
       <h1>Projects</h1>
-      {data.allContentfulProjects.edges.map(({ node }) => (
-        <h2>{node.name}</h2>
-      ))}
+      {data.allContentfulProjects.edges.map(({ node }) => {
+        let slug = slugify(node.name);
+        return (
+          <Link to={`/projects/${slug}`}>
+            <h2>{node.name}</h2>
+          </Link>
+        )
+      })}
     </Layout>
   )
 }
