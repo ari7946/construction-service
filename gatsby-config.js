ba8d86bb0b4b77aef18500fb1a6accf3f495551f
ari7946 <mercado7946@gmail.com>
2020-10-18 15:07:09 -0700

    Add images to source file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,13 @@ module.exports = {
         path: `${__dirname}/src/`,
       }
     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/images`,
+      }
+    },
     'gatsby-plugin-sharp',
     { 
       resolve: 'gatsby-transformer-remark',
