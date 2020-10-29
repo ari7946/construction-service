21b9f3fa4529c60236068d37f41cc8b51dde926a
ari7946 <mercado7946@gmail.com>
2020-10-29 14:45:37 -0700

    Add general styling to various components including footer

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -23,19 +23,21 @@ const BlogPage = () => {
   return (
     <Layout>
       <Head title="Blog" />
-      <h1>Blog</h1>
-      <ol className={blogStyles.posts}>
-        {data.allContentfulBlogPost.edges.map((edge) => {
-          return (
-            <li className={blogStyles.post}>
-              <Link to={`/blog/${edge.node.slug}`}>
-                <h2>{edge.node.title}</h2>
-                <p>{edge.node.publishedDate}</p>
-              </Link>
-            </li>
-          )
-        })}
-      </ol>
+      <div className={blogStyles.blogPageContainer}>
+        <h1>Blog</h1>
+        <ol className={blogStyles.posts}>
+          {data.allContentfulBlogPost.edges.map((edge) => {
+            return (
+              <li className={blogStyles.post}>
+                <Link to={`/blog/${edge.node.slug}`}>
+                  <h2>{edge.node.title}</h2>
+                  <p>{edge.node.publishedDate}</p>
+                </Link>
+              </li>
+            )
+          })}
+        </ol>
+      </div>
     </Layout>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import Layout from "../components/layout/layout"
 import Head from "../components/head"
+import ContactInfo from '../components/contact-info/contact-info'
+
+import contactStyles from './contact.module.scss'
 
 const ContactPage = () => {
   return (
     <Layout>
-      <Head title="Contact" />
-      <h1>Contact</h1>
+      <div className={contactStyles.contactPageContainer}>
+        <Head title="Contact" />
+        <h1>Contact</h1>
+        <ContactInfo />
+      </div>
     </Layout>
   )
 }
