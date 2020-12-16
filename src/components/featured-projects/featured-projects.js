32fc7c1a7e82a569da9e0feaeccfc3db81a51c5d
ari7946 <mercado7946@gmail.com>
2020-12-15 19:42:17 -0800

    Create queries and implement gatsby-image plugin loading functionality for the home page featured projects

--- src/components/featured-projects/featured-projects.js
+++ src/components/featured-projects/featured-projects.js
@@ -1,65 +1,150 @@
 import React from "react"
-import acapulco from '../../images/acapulco1.jpg'
-import ave12 from "../../images/ave-121.jpg"
-import chevys from "../../images/chevys1.jpg"
-import lasBrisas from "../../images/las-brisas1.jpg"
-import schoolRoof from "../../images/school-roof1.jpg"
-import floorReinforcement from '../../images/floor-reinforcement1.jpg'
-import whoSong from "../../images/whosong1.jpg"
-import elTorito from "../../images/el-torito1.jpg"
-
+import Img from 'gatsby-image';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import featuredProjectsStyles from './featured-projects.module.scss';
 
 const FeaturedProjects = () => {
+  const images = useStaticQuery(graphql`
+    query {
+      acapulco: file(relativePath: { eq: "images/acapulco3.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      ave12: file(relativePath: { eq: "images/ave-121.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      chevys: file(relativePath: { eq: "images/chevys1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      lasBrisas: file(relativePath: { eq: "images/las-brisas1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      schoolRoof: file(relativePath: { eq: "images/school-roof1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      floorReinforcement: file(relativePath: { eq: "images/floor-reinforcement1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      whoSong: file(relativePath: { eq: "images/whosong1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+
+      elTorito: file(relativePath: { eq: "images/el-torito1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+`)
+
   return (
     <section>
       <div className={featuredProjectsStyles.featuredProjects}>
         <div className={featuredProjectsStyles.colOne}>
           <div className={featuredProjectsStyles.imgContainer}>
-            <img src={acapulco} alt="Acapulco restaurant" />
-            <div className={featuredProjectsStyles.title}>Las Brisas</div>
+=            <Img 
+              fluid={images.acapulco.childImageSharp.fluid} 
+              className={featuredProjectsStyles.featuredImage}
+            />
+            {/* <div>Acapulco</div> */}
           </div>
 
           <div className={featuredProjectsStyles.imgContainer}>
-            <img src={schoolRoof} alt="school roof" />
-            <div>School Roof</div>
+            <Img 
+              fluid={images.schoolRoof.childImageSharp.fluid} 
+              className={featuredProjectsStyles.featuredImage}
+            />
+            {/* <div>School Roof</div> */}
           </div>
         </div>
 
         <div className={featuredProjectsStyles.colTwo}>
           <div className={featuredProjectsStyles.imgContainer}>
-            <img src={lasBrisas} alt="Las Brisas restaurant" />
-            <div>Las Brisas</div>
+            <Img 
+              fluid={images.lasBrisas.childImageSharp.fluid} 
+              className={featuredProjectsStyles.featuredImage}
+            />
+            {/* <div>Las Brisas</div> */}
           </div>
 
           <div className={featuredProjectsStyles.imgContainer}>
-            <img src={floorReinforcement} alt="floor reinforcement" />
-            <div>Floor Reinforcement</div>
+            <Img 
+              fluid={images.floorReinforcement.childImageSharp.fluid} 
+              className={featuredProjectsStyles.featuredImage}
+            />
+            {/* <div>Floor Reinforcement</div> */}
           </div>
         </div>
 
         <div className={featuredProjectsStyles.colThree}>
           <div className={featuredProjectsStyles.imgContainer}>
-            <img src={chevys} alt="chevy's restaurant" />
-            <div>Chevy's</div>
+            <Img 
+              fluid={images.chevys.childImageSharp.fluid} 
+              className={featuredProjectsStyles.featuredImage}
+            />
+            {/* <div>Chevy's</div> */}
           </div>
 
           <div className={featuredProjectsStyles.imgContainer}>
-            <img src={whoSong} alt="Who Song and Larry's restaurant" />
-            <div>Who Song and Larry's</div>
+            <Img 
+              fluid={images.whoSong.childImageSharp.fluid} 
+              className={featuredProjectsStyles.featuredImage}
+            />
+            {/* <div>Who Song and Larry's</div> */}
           </div>
         </div>
 
         <div className={featuredProjectsStyles.colFour}>
           <div className={featuredProjectsStyles.imgContainer}>
-            <img src={ave12} alt="Ave12" />
-            <div>Bath Remodel</div>
+            <Img 
+              fluid={images.ave12.childImageSharp.fluid} 
+              className={featuredProjectsStyles.featuredImage}
+            />
+            {/* <div>Bath Remodel</div> */}
           </div>
 
           <div className={featuredProjectsStyles.imgContainer}>
-            <img src={elTorito} alt="El Torito Restaurant" />
-            <div>El Torito</div>
+            <Img 
+              fluid={images.elTorito.childImageSharp.fluid} 
+              className={featuredProjectsStyles.featuredImage}
+            />
+            {/* <div>El Torito</div> */}
           </div>
         </div>
       </div>
