bc3580205ee4e48541d44f6a0a229687ec543688
ari7946 <mercado7946@gmail.com>
2020-12-27 22:19:18 -0800

    Cap see more projects CTA and add icon

--- src/components/block-button/block-button.js
+++ src/components/block-button/block-button.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'gatsby';
 import buttonStyles from './block-button.module.scss';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons"
+
 const BlockButton = props => {
   return (
     <button className={buttonStyles.buttonContainer}>
@@ -9,6 +12,7 @@ const BlockButton = props => {
         to={props.location}
       >
         {props.text}
+        {' '}<FontAwesomeIcon icon={faAngleDoubleRight}/>
       </Link>
     </button>
   )
