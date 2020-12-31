9cb093b02b5345956c0eb6b9591116dffbdb1d87
ari7946 <mercado7946@gmail.com>
2020-12-31 12:14:05 -0800

    Refactor personal component

--- src/components/personnel/personnel.js
+++ src/components/personnel/personnel.js
@@ -7,25 +7,29 @@ import personnelStyles from './personnel.module.scss';
 const Personnel = () => {
   return (
     <section className={personnelStyles.personnel}>
-      <div className={personnelStyles.personCard}>
+      <article className={personnelStyles.personCard}>
         <img src={carlos} alt="CARLOS SALAZAR" />
-        <h4>CARLOS SALAZAR</h4>
-        <h5>New Projects Manager</h5>
+        <hgroup>
+          <h4>CARLOS SALAZAR</h4>
+          <h5>New Projects Manager</h5>
+        </hgroup>
         <p className={personnelStyles.description}>
           Carlos has 6 years of experience in wood framing, demo, drywall,
           concrete work, painting, tile work and estimating.
         </p>
-      </div>
+      </article>
 
-      <div className={personnelStyles.personCard}>
+      <article className={personnelStyles.personCard}>
         <img src={victor} alt="VICTOR LOPEZ" />
-        <h4>VICTOR LOPEZ</h4>
-        <h5>General Projects Manager</h5>
+        <hgroup>
+          <h4>VICTOR LOPEZ</h4>
+          <h5>General Projects Manager</h5>
+        </hgroup>
         <p className={personnelStyles.description}>
           Victor has 16 years of experience in metal and wood framing, drywall,
           concrete work, demo, painting, plumbing and project management.
         </p>
-      </div>
+      </article>
     </section>
   )
 }
